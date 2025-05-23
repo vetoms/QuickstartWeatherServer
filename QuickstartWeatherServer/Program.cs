5e51d41aa346b9136a8b508ca407b3f04559ba2b
Daniel <Daniel@DANIELRF>
2025-05-23 16:12:36 -0600

    Fix Issue JSON

--- QuickstartWeatherServer/Program.cs
+++ QuickstartWeatherServer/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System.Net.Http.Headers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using QuickstartWeatherServer;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddMcpServer()
     .WithStdioServerTransport()
     .WithTools<WeatherTools>();
+
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
 
 await builder.Build().RunAsync();
